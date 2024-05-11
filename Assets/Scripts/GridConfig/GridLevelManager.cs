8129cdeac15365907c4b577aa6abea5f3a0a9291
Devon Oberdan <devonoberdan@gmail.com>
2024-05-10 23:32:15 -0400

    Fixed WebGL issues, fully playable now

--- Assets/Scripts/GridConfig/GridLevelManager.cs
+++ Assets/Scripts/GridConfig/GridLevelManager.cs
@@ -53,7 +53,12 @@ public class GridLevelManager : MonoBehaviour
         if (startingCustom)
             levelIndex = -1;
         else
-            LevelIndex = 0;
+            Invoke(nameof(StartGame), 0.1f);
+    }
+
+    private void StartGame()
+    {
+        LevelIndex = 0;
     }
 
     private void Update()
