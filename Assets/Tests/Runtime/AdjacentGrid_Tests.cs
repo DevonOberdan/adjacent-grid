d68a7c59eaf3e1cbff8178d53c4f7b5c08b6c3cb
Devon Oberdan <devonoberdan@gmail.com>
2025-01-10 12:23:32 -0500

    Three unit tests, running continuously and passing

--- Assets/Tests/Runtime/AdjacentGrid_Tests.cs
+++ Assets/Tests/Runtime/AdjacentGrid_Tests.cs
@@ -8,18 +8,30 @@ using UnityEngine.TestTools;
 
 public class AdjacentGrid_Tests
 {
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator AdjacentGrid_CorrectSolutionCountFound()
+    public IEnumerator AdjacentGrid_CorrectSolutionCountFound_Level1()
     {
         yield return RunBot(0);
-
         GridManager gridManager = GameObject.FindAnyObjectByType<GridManager>();
-        int count = gridManager.PuzzleConfig.SolutionCount;
+        Assert.AreEqual(8, gridManager.PuzzleConfig.SolutionCount);
+        yield return null;
+    }
 
-        Assert.AreEqual(8, count);
+    [UnityTest]
+    public IEnumerator AdjacentGrid_CorrectSolutionCountFound_Level4()
+    {
+        yield return RunBot(3);
+        GridManager gridManager = GameObject.FindAnyObjectByType<GridManager>();
+        Assert.AreEqual(14, gridManager.PuzzleConfig.SolutionCount);
+        yield return null;
+    }
 
+    [UnityTest]
+    public IEnumerator AdjacentGrid_CorrectSolutionCountFound_Level22()
+    {
+        yield return RunBot(21);
+        GridManager gridManager = GameObject.FindAnyObjectByType<GridManager>();
+        Assert.AreEqual(19, gridManager.PuzzleConfig.SolutionCount);
         yield return null;
     }
 
@@ -33,14 +45,12 @@ public class AdjacentGrid_Tests
         levelManager.SetLevelIndex(level);
 
         yield return new WaitForEndOfFrame();
-
         yield return solutionBot.StartCoroutine(solutionBot.SolvePuzzle());
     }
 
     private IEnumerator GetToGame()
     {
-        yield return new WaitForSeconds(0.1f);
-        SceneManager.LoadScene(0);
+        yield return SceneManager.LoadSceneAsync(0);
         yield return new WaitForSeconds(.25f);
         GameObject.FindAnyObjectByType<SceneLoadRequester>().Request();
         yield return new WaitForSeconds(.25f);
