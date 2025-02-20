cab66cc342a98861b3b7ce2235a4cb5e39bb0627
Devon Oberdan <devonoberdan@gmail.com>
2025-02-19 19:14:55 -0500

    Removed  field from PrefabContainerSO

--- Assets/Scripts/Utilities/PrefabContainerSO.cs
+++ Assets/Scripts/Utilities/PrefabContainerSO.cs
@@ -3,7 +3,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName =nameof(PrefabContainerSO), menuName =nameof(PrefabContainerSO), order = 0)]
 public class PrefabContainerSO : ScriptableObject
 {
-    public GridPiece PiecePrefab;
-
     public GameObject Prefab;
 }
